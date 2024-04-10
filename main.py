0aefb0876da10aa55a57bfb17459ee43eb7bbfeb
FADGYAS GEORGE ANDREI <fadgyasgeorgeandrei@FADGYASs-MBP.fritz.box>
2024-04-10 13:20:32 +0200

    Add tests for find_substring function

--- main.py
+++ main.py
@@ -18,3 +18,29 @@ def find_substring(text, substring):
             indices.append(i)
 
     return indices
+
+
+def test_find_substring():
+
+    try:
+        find_substring(123, "test")
+    except TypeError as e:
+        assert str(e) == "Both string and substring should be strings", "Test for wrong input failed"
+
+
+    try:
+        find_substring("test string", "")
+    except ValueError as e:
+        assert str(e) == "Substring cannot be empty", "Test for an empty string failed"
+
+
+    assert find_substring("test string test", "test") == [0, 12], "Additional test failed"
+
+
+def main():
+    find_substring("test string test", "test")
+    test_find_substring()
+
+
+if __name__ == "__main__":
+    main()
